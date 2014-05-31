3c4dee38a3bf77f79aa00a4c9825e5a2cc034c3b
Piotr Strugacz <piotr.strugacz@xsolve.pl>
2014-05-31 18:33:25 +0200

    Controllers

---
+++ src/Tatusiowo/DemoBundle/Controller/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tatusiowo\DemoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class UserController extends Controller
+{
+    /**
+     * @Route("/data", name="tatusiowo_user_data")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Template()
+     */
+    public function dataAction()
+    {
+        return [];
+    }
+}
